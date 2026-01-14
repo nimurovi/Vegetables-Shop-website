e89ab7e694362c5abfac31fea2037d2cb680a441
nimurovi <mdnimurhossenovi@gmail.com>
2026-01-14 23:14:14 +0600

    services done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+module.exports = {
+  content: [
+    "./*.html",
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
